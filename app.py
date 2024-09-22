cdf6e7c7fef004e2d4ae2e85b4f783c0b26c336d
lynn-shehab <lynnshehab12@gmail.com>
2024-09-22 16:33:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,11 +74,11 @@ aggregation_level = st.radio("Select Aggregation Level:", ('Town Level', 'Facili
 
 if aggregation_level == 'Town Level':
   sunburst_chart = px.sunburst(filtered_sunburst_data,
-                              path=['Town', 'Facility'],
-                              values='Total',
-                              title=f'Sunburst Chart of Tourism Facilities by Town (Min {total_facility_threshold} Total Facilities)',
-                              color='Facility',
-                              color_continuous_scale=color_scheme)
+                               path=['Town', 'Facility'],
+                               values='Total',
+                               title=f'Sunburst Chart of Tourism Facilities by Town (Min {total_facility_threshold} Total Facilities)',
+                               color='Facility',
+                               color_continuous_scale=color_scheme)
 
 
 
@@ -91,6 +91,7 @@ else:
                                  color_continuous_scale=color_scheme)
 
 st.plotly_chart(sunburst_chart)
+
 
 st.write("### Insights:")
 st.write("- By using the interactivity of the visualizations of the dataset, we can understand that retaurants are the most prevalent facility type among guest houses, cafes, and hotels.")
